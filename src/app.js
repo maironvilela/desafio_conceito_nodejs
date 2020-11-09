7ca30dfbaa1b0371135b71cee23accb7f0df9783
Mairon Vilela <maironvilela@hotmail.com>
2020-11-08 22:28:59 -0300

    Finalização do desafio 02 do GoStack Bootcamp

--- src/app.js
+++ src/app.js
@@ -1,33 +1,98 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+/* Middleware que intercepta requisições que necessita verificar se o ID passado 
+como parametro é um id válido */
+
+function repositoryIdValidate(request, response, next){
+
+  const { id } = request.params;
+
+  if(!isUuid(id)){
+    return response.status(400).json({error: "repository not found"})
+  }
+
+  return next();
+
+}
+
+
 const repositories = [];
 
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
+
 
 app.post("/repositories", (request, response) => {
-  // TODO
+
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url, 
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository)
+
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+
+
+app.put("/repositories/:id",repositoryIdValidate, (request, response) => {
+
+  const {title, url, techs} = request.body;
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex (repository => repository.id == id);
+
+  repositories[repositoryIndex] = {...repositories[repositoryIndex], title, url, techs}
+
+    
+  return response.status(200).json(repositories[repositoryIndex])
+   
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+
+app.delete("/repositories/:id", repositoryIdValidate,  (request, response) => {
+
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex (repository => repository.id == id);
+ 
+  repositories.splice(repositoryIndex,1);
+ 
+  return response.status(204).json();  
+ 
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+
+app.post("/repositories/:id/like", repositoryIdValidate, (request, response) => {
+
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id)
+
+  const likes = repositories[repositoryIndex].likes+1;
+  console.log(likes)
+
+  repositories[repositoryIndex] = {...repositories[repositoryIndex], likes: likes}
+
+  return response.status(201).json(repositories[repositoryIndex]);
 });
+
 
 module.exports = app;
